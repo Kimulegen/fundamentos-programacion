245bdb357e9a1b5d7f476e0067ddc4cc4e94d88c
Kimulegen <137565888+Kimulegen@users.noreply.github.com>
2024-05-31 21:50:34 -0400

    chore: rename file, add comments

--- experiencia_03/3.1/3.1.2/01_mostrar_nombre_largo.py
+++ experiencia_03/3.1/3.1.2/01_mostrar_nombre_largo.py
@@ -1,9 +1,16 @@
+#! Python3
+# Ask for 3 names, return the longest
+
+# initialize variables
 names = []
 longest_name = ''
+
+# user inputs 3 names, store longest name
 for i in range(3):
     name = input('Ingresa un nombre\n> ')
     names.append(name)
     longest_name = name if len(name) >= len(longest_name) else longest_name
 
+# print longest name
 print(f' el nombre más largo es : {longest_name}')
 

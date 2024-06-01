5fb69c3345a98a4a0753adf2f3663f9dc38ebb6d
Kimulegen <137565888+Kimulegen@users.noreply.github.com>
2024-05-31 21:54:24 -0400

    chore: add comments

--- experiencia_03/3.1/3.1.2/02_nombres_y_apellidos.py
+++ experiencia_03/3.1/3.1.2/02_nombres_y_apellidos.py
@@ -1,5 +1,10 @@
+#! Python3
+# Show 3 full names
+
+# initialize variables
 names = ['Pedro', 'Juan', 'Diego']
 last_names = ['Salazar', 'Morales', 'Jerez']
 
+# show first name and last name
 for i in range(3):
     print(names[i], last_names[i])

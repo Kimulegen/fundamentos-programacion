6eb9776ce8e5610e47e935cc3d626aaf76b47ba4
Kimulegen <137565888+Kimulegen@users.noreply.github.com>
2024-05-31 21:30:17 -0400

    feat: add user input for building matrix

--- experiencia_03/3.1/3.1.3/01_mostrar_elementos.py
+++ experiencia_03/3.1/3.1.3/01_mostrar_elementos.py
@@ -1,8 +1,18 @@
-matrix = [
+matrix_example = [
     [1,2,3,4],
     [5,6,7,8],
     [9,10,11,12]
 ]
+
+rows = int(input("Ingrese el número de filas de su matriz"))
+cols = int(input("Ingrese el número de columnas de su matriz"))
+matrix = []
+
+for i in range(rows)
+    row = []
+    for j in range(cols)
+        row.append(input(f"Ingrese el número de la fila {i} y la columna {j}"))
+    matrix.append(row)
 
 for row in matrix:
     for element in row:

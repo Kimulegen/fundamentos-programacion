c49e69412eac543f0ba84e9c3331cf4cd1362648
Kimulegen <137565888+Kimulegen@users.noreply.github.com>
2024-06-05 21:39:48 -0400

    chore: add comments

--- experiencia_03/3.1/3.1.3/03_bus.py
+++ experiencia_03/3.1/3.1.3/03_bus.py
@@ -1,6 +1,11 @@
+#! Python3
+# print the sits of a bus
+
+# initialize variables
 bus = []
 sit_number = 1
 
+# build bus, each sit is represented as a number
 for i in range(10):
     row = []
     for j in range(4):
@@ -8,6 +13,7 @@ for i in range(10):
         row.append(sit)
     bus.append(row)
 
+# show formatted bus matrix
 for row in bus:
     for i,item in enumerate(row):
         print(item, end=' ')

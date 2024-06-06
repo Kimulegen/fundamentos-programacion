bbdd80c871a6dbfa551313cc18d55ff98d3d154c
Kimulegen <137565888+Kimulegen@users.noreply.github.com>
2024-06-05 21:23:59 -0400

    chore: add comments and clear

--- experiencia_03/3.1/3.1.2/05_supermercado.py
+++ experiencia_03/3.1/3.1.2/05_supermercado.py
@@ -1,9 +1,13 @@
 #! Python3
 # Supermarket simulator, where you can add products, see your products and see the total cost of the purchase
 
+# importing os to use clear
+import os
+
 # initialize variables
 canasta = {}
 total = 0
+product = ''
 
 # main menu
 while True:
@@ -17,6 +21,7 @@ while True:
 
     # add products feature
     if choice == '1':
+        os.system('cls||clear')
         print('Seleccione producto a agregar')
         print('1. Arroz $2350')
         print('2. Tallarines $1190')
@@ -30,31 +35,41 @@ while True:
             canasta.setdefault('arroz', 0)
             canasta['arroz'] += 1
             total += 2350
+            product = 'arroz'
         elif product == '2':
             canasta.setdefault('tallarines', 0)
             canasta['tallarines'] += 1
             total += 1190
+            product = 'tallarines'
         elif product == '3':
             canasta.setdefault('aceite', 0)
             canasta['aceite'] += 1
             total += 2290
+            product = 'aceite'
         if product == '4':
             canasta.setdefault('mermelada', 0)
             canasta['mermelada'] += 1
             total += 1450
+            product = 'mermelada'
         if product == '5':
             canasta.setdefault('cereal', 0)
             canasta['cereal'] += 1
             total += 2690
+            product = 'cereal'
 
+        os.system('cls||clear')
+        print(f"{product} añadido con éxito")
+        
     # print all products feature
     elif choice == '2':
+        os.system('cls||clear')
         for k,v in canasta.items():
             print(f'{v} {k}')
         print()
 
     # show total feature
     elif choice == '3':
+        os.system('cls||clear')
         print(f'Su total a pagar es: {total}')
         print()
 

c824cfad11e9470a05b5f95a5af5a6aac69b4792
Kimulegen <137565888+Kimulegen@users.noreply.github.com>
2024-06-05 21:31:34 -0400

    fix: fix for loops, add commnets, add clear

--- experiencia_03/3.1/3.1.3/01_mostrar_elementos.py
+++ experiencia_03/3.1/3.1.3/01_mostrar_elementos.py
@@ -1,6 +1,9 @@
 #! Python3
 # Ask for a matrix, show formatted matrix
 
+# importing os for clear
+import os
+
 matrix_example = [
     [1,2,3,4],
     [5,6,7,8],
@@ -8,18 +11,21 @@ matrix_example = [
 ]
 
 # ask for size of matrix
-rows = int(input("Ingrese el número de filas de su matriz"))
-cols = int(input("Ingrese el número de columnas de su matriz"))
+rows = int(input("Ingrese el número de filas de su matriz\n> "))
+cols = int(input("Ingrese el número de columnas de su matriz\n> "))
 matrix = []
 
+os.system('cls||clear')
 # build matrix
-for i in range(rows)
+for i in range(rows):
     row = []
-    for j in range(cols)
-        row.append(input(f"Ingrese el número de la fila {i} y la columna {j}"))
+    for j in range(cols):
+        row.append(input(f"Ingrese el número de la fila {i+1} y la columna {j+1}\n> "))
+        os.system('cls||clear')
     matrix.append(row)
 
 # show matrix
+print('Su matriz:')
 for row in matrix:
     for element in row:
         print(element, end=' ')

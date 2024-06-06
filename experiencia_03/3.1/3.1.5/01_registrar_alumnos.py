0f91adbdf810ba035bb5f72c13bf5730eefb1f85
kimulegen <kimulegen@proton.me>
2024-06-06 19:24:19 -0400

    chore: add comments

--- experiencia_03/3.1/3.1.5/01_registrar_alumnos.py
+++ experiencia_03/3.1/3.1.5/01_registrar_alumnos.py
@@ -1,23 +1,32 @@
+#! Python3
+# Add students to a class
+
+import os
+
+# initialize variables
 curso = []
 counter = 0
+
+# add students
 while counter < 30:
     if counter == 29:
-        print('No hay mas cupos en este curso')
+        print("No hay mas cupos en este curso")
 
     alumno = {}
-    alumno['name'] = input('Ingrese el nombre del alumno: ')
-    alumno['address'] = input('Ingrese la dirección del alumno: ')
-    alumno['phone'] = input('Ingrese el teléfono del alumno: ')
+    alumno["name"] = input("Ingrese el nombre del alumno: ")
+    alumno["address"] = input("Ingrese la dirección del alumno: ")
+    alumno["phone"] = input("Ingrese el teléfono del alumno: ")
     curso.append(alumno)
 
-    choice = input('Desea agregar otro alumno (s/n): ')
-    if choice == 'n':
+    choice = input("Desea agregar otro alumno (s/n): ")
+    if choice == "n":
+        os.system("cls||clear")
         break
 
+# show students
+print(" Alumnos ".center(30, "="))
 for alumno in curso:
-    print()
     print(f'Nombre: {alumno["name"]}')
     print(f'Dirección: {alumno["address"]}')
     print(f'Teléfono: {alumno["phone"]}')
-    
-        
+    print()
--- experiencia_03/3.1/3.1.5/02_sorteos.py
+++ experiencia_03/3.1/3.1.5/02_sorteos.py
@@ -1,27 +1,37 @@
+#! Python3
+# Lottery simulator
+
+import os
 import random
 
+# initialize variables
 user_ticket = []
-print('Ingrese sus 7 números de la suerte')
+
+# ask user for 7 lucky numbers, show them
+print("Ingrese sus 7 números de la suerte")
 for i in range(7):
-    user_ticket.append(int(input(f'Ingrese número: ')))
+    user_ticket.append(int(input(f"Ingrese número: ")))
+os.system("cls||clear")
+print("Usted ingresó los siguientes números:", user_ticket)
 
-print('Usted ingresó los siguientes números:', user_ticket)
-
+# 3 games, each one with different winner numbers
 for round in range(3):
-    print(f'Los números sorteados en la ronda {round + 1} fueron')
+    print(f"Los números sorteados en la ronda {round + 1} fueron")
 
+    # pick random winner numbers
     lottery = []
     for i in range(7):
         while True:
-            number = random.randint(1,49)
-            if (lottery.count(number) == 0):
+            number = random.randint(1, 49)
+            if lottery.count(number) == 0:
                 lottery.append(number)
                 break
-        print(lottery[len(lottery)-1])
-    
-    if (sorted(user_ticket) == sorted(lottery)):
-        print('Ganaste!')
+        print(number)
+
+    # compare winner numbers with user numbers
+    if sorted(user_ticket) == sorted(lottery):
+        print("Ganaste!")
     else:
-        print('Lo siento, pero no has ganado en esta ronda')
-    
+        print("Lo siento, pero no has ganado en esta ronda")
+
     print()

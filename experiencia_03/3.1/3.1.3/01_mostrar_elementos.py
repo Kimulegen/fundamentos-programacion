7a2b8465001336180450ba90ad3e43330481eb78
Kimulegen <137565888+Kimulegen@users.noreply.github.com>
2024-06-05 21:25:59 -0400

    chore: add comments

--- experiencia_03/3.1/3.1.3/01_mostrar_elementos.py
+++ experiencia_03/3.1/3.1.3/01_mostrar_elementos.py
@@ -1,19 +1,25 @@
+#! Python3
+# Ask for a matrix, show formatted matrix
+
 matrix_example = [
     [1,2,3,4],
     [5,6,7,8],
     [9,10,11,12]
 ]
 
+# ask for size of matrix
 rows = int(input("Ingrese el número de filas de su matriz"))
 cols = int(input("Ingrese el número de columnas de su matriz"))
 matrix = []
 
+# build matrix
 for i in range(rows)
     row = []
     for j in range(cols)
         row.append(input(f"Ingrese el número de la fila {i} y la columna {j}"))
     matrix.append(row)
 
+# show matrix
 for row in matrix:
     for element in row:
         print(element, end=' ')

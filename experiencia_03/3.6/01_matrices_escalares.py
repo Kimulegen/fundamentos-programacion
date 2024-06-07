cbe39d4b3dd253a061f49416eb8898a73d686762
kimulegen <kimulegen@proton.me>
2024-06-07 15:57:21 -0400

    chore: format, add comments, add clears

--- experiencia_03/3.6/01_matrices_escalares.py
+++ experiencia_03/3.6/01_matrices_escalares.py
@@ -1,23 +1,30 @@
+#! Python3
+# A program where you can sum matrices, multiply two matrices, and multiply a matrix by a scalar
 
-# permite multiplicar elementos de una matriz ingresadas por el usuario.
-# Usar while for matrices trt/except validaciones funciones
-# Menu de opciones para poder sumar matrices, multiplicar matrices y un boton para salir
+import os
+
+
+# format and print a matrix
 def pretty_print(matrix):
     for row in matrix:
         print(" ".join(f"{num:4}" for num in row))
 
-def get_matrix_dimensions(matrix):
-    return (len(matrix),len(matrix[0]))
 
+# get matrix dimensions
+def get_matrix_dimensions(matrix):
+    return (len(matrix), len(matrix[0]))
+
+
+# sum two matrices
 def sum_matrices(matrix1, matrix2):
     # Check matrices dimensions
-    m1_dim= get_matrix_dimensions(matrix1)
-    m2_dim= get_matrix_dimensions(matrix2)
+    m1_dim = get_matrix_dimensions(matrix1)
+    m2_dim = get_matrix_dimensions(matrix2)
     if m1_dim != m2_dim:
-        print('Las dimensiones no coinciden!')
+        print("Las dimensiones no coinciden!")
         return 0
 
-    #sum
+    # sum
     result = []
     for i in range(m1_dim[0]):
         row = []
@@ -27,14 +34,16 @@ def sum_matrices(matrix1, matrix2):
 
     return result
 
-def multiply_matrices(matrix1,matrix2):
+
+# multiply two matrices
+def multiply_matrices(matrix1, matrix2):
     # Check dimensions
     m1_dim = get_matrix_dimensions(matrix1)
     m2_dim = get_matrix_dimensions(matrix2)
     if m1_dim[1] != m2_dim[0]:
-        print('las dimensiones no coinciden')
+        print("las dimensiones no coinciden!")
         return 0
-    
+
     # Multiply
     result = []
     for i in range(m1_dim[0]):
@@ -42,120 +51,142 @@ def multiply_matrices(matrix1,matrix2):
         for j in range(m2_dim[1]):
             sum = 0
             for k in range(m1_dim[1]):
-                sum += (matrix1[i][k] * matrix2[k][j])
+                sum += matrix1[i][k] * matrix2[k][j]
             row.append(sum)
         result.append(row)
-    
+
     return result
 
-def multiply_matrix_scalar(matrix,scalar):
+
+# multiply a matrix by a scalar
+def multiply_matrix_scalar(matrix, scalar):
     result = []
     for row in matrix:
         new_row = []
         for i in row:
-            new_row.append(i*scalar)
+            new_row.append(i * scalar)
         result.append(new_row)
-    
+
     return result
 
-# def is_matrix_valid(matrix):
-#     for row in matrix:
-#         for i in row:
-#             try:
-#                 int(i)
-#             except ValueError:
-#                 return False
-#     return True
 
+# build a matrix, ask for dimensions and values
 def build_matrix():
     try:
-        n_rows = int(input('Ingrese el numero de filas de su matriz\n> '))
-        n_columns = int(input('Ingrese el numero de columnas de su matriz\n> '))
+        n_rows = int(input("Ingrese el número de filas de su matriz\n> "))
+        n_columns = int(input("Ingrese el número de columnas de su matriz\n> "))
 
         matrix = []
         for i in range(n_rows):
-            row =[]
+            row = []
             for j in range(n_columns):
-                row.append(int(input(f'Ingrese el dato de la fila {i+1} y la columna {j+1}\n> ')))
+                os.system("cls||clear")
+                row.append(
+                    int(
+                        input(
+                            f"Ingrese el dato de la fila {i+1} y la columna {j+1}\n> "
+                        )
+                    )
+                )
             matrix.append(row)
-        print('Su matriz construida es:')
+        os.system("cls||clear")
+        print("Su matriz construida es:")
         pretty_print(matrix)
         print()
         return matrix
     except ValueError:
-        print('Error al construir su matriz, ingreso un dato no numerico')
+        print("Error al construir su matriz, ingresó un dato no numérico")
         return 0
 
+
+# menu
 def main():
     while True:
-        print('Bienvenido al menú')
-        print('1. Sumar dos matrices')
-        print('2. Multiplicar dos matrices')
-        print('3. Multiplicar matriz por escalar')
-        print('4. Salir')
+        print(" Bienvenido al menú ".center(40, "="))
+        print("1. Sumar dos matrices")
+        print("2. Multiplicar dos matrices")
+        print("3. Multiplicar matriz por escalar")
+        print("4. Salir")
         try:
-            choice = int(input('> '))
+            choice = int(input("> "))
         except ValueError:
-            print('\nError, eleccion invalida\n')
+            os.system("cls||clear")
+            print("\nError, elección inválida\n")
             continue
 
+        # sum two matrices
         if choice == 1:
+            os.system("cls||clear")
             m1 = build_matrix()
             m2 = build_matrix()
 
             if m1 == 0 or m2 == 0:
-                print('No se pudo sumar las matrices')
+                print("No se pudo sumar las matrices\n")
                 continue
 
-            sum = sum_matrices(m1,m2)
+            sum = sum_matrices(m1, m2)
 
             if sum == 0:
-                print('No se pudo sumar las matrices')
+                print("No se pudo sumar las matrices\n")
                 continue
 
-            print('La suma de ambas matrices es:')
+            os.system("cls||clear")
+            print("La suma de ambas matrices es:")
             pretty_print(sum)
             print()
+
+        # multiply two matrices
         elif choice == 2:
+            os.system("cls||clear")
             m1 = build_matrix()
             m2 = build_matrix()
 
             if m1 == 0 or m2 == 0:
-                print('No se pudo multiplicar las matrices')
+                print("No se pudo multiplicar las matrices")
                 continue
 
-            mult = multiply_matrices(m1,m2)
+            mult = multiply_matrices(m1, m2)
 
             if mult == 0:
-                print('No se pudo multiplicar las matrices')
+                print("No se pudo multiplicar las matrices")
                 continue
 
-            print('La multiplicacion de ambas matrices es:')
+            os.system("cls||clear")
+            print("La multiplicación de ambas matrices es:")
             pretty_print(mult)
             print()
+
+        # multiply a matrix by a scalar
         elif choice == 3:
+            os.system("cls||clear")
             m1 = build_matrix()
             try:
-                scalar = int(input('Ingrese el escalar\n> '))   
+                scalar = int(input("Ingrese el escalar\n> "))
             except ValueError:
-                print('Escalar ingresado no es un dato numerico')
+                print("Escalar ingresado no es un dato numérico")
                 continue
 
             if m1 == 0:
-                print('No se pudo multiplicar las matriz')
+                print("No se pudo multiplicar las matriz")
                 continue
 
-            mult = multiply_matrix_scalar(m1,scalar)
+            mult = multiply_matrix_scalar(m1, scalar)
 
-            print('La multiplicacion de la matriz por el escalar es:')
+            os.system("cls||clear")
+            print("La multiplicación de la matriz por el escalar es:")
             pretty_print(mult)
             print()
+
+        # exit program
         elif choice == 4:
-            print('Adiós')
+            print("Adiós")
             break
+
+        # handle invalid options
         else:
-            print('\nSeleccione una opcion valida\n')
+            os.system("cls||clear")
+            print("\nSeleccione una opción válida\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- experiencia_03/3.6/02_acciones_auto.py
+++ experiencia_03/3.6/02_acciones_auto.py
@@ -1,63 +1,80 @@
-# acciones
-# encender auto, colocar alarma, auto encendido, auto estacionado, auto con nivel de aceite bajo
-# usar while for listas try/except validaciones, archivo CSV y funciones.
-# menu de opciones para poder agregar acciones a la bitacora texto:
-# la segunda opcion es para ver las acciones hechas en el auto
-# la tercera opcion guarda la bitacora en un archivo CSV 
-# la cuarta opcion es para salir
-# Las acciones se registran con la fecha y hora actual en el formato 'dd-mm-yyyy HH:MM:SS' utilizar libreria from datetime import datetime
-# El usuario debe guardar el archivo CSV con el nombre y extension
-# Si el archivo CSV ya existe, se debe sobreescribir
+#! Python3
+# Generate, show, and save a car log
+
+import os
 import csv
 from datetime import datetime
 
+# initialize variables
 bitacora = []
 
+
+# menu
 def main():
     while True:
-        print('*'*15,'Bienvenido al menú, seleccione su opción','*'*15)
-        print('1. Agregar acción')
-        print('2. Ver bitácora')
-        print('3. Guardar bitácora')
-        print('4. Salir')
+        print(" Bienvenido al menú, seleccione su opción ".center(60, "="))
+        print("1. Agregar acción")
+        print("2. Ver bitácora")
+        print("3. Guardar bitácora")
+        print("4. Salir")
         try:
-            choice = int(input('> '))
+            choice = int(input("> "))
         except ValueError:
-            print()
-            print('Error, favor ingrese un dato numérico')
-            
+            os.system("cls||clear")
+            print("\nError, favor ingrese un dato numérico\n")
+            continue
+
         print()
+
+        # add an entry to the log
         if choice == 1:
-            action = input('¿Qué acción desea agregar?\n> ')
-            formatted_datetime = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+            os.system("cls||clear")
+            action = input("¿Qué acción desea agregar?\n> ")
+            formatted_datetime = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
             bitacora.append([formatted_datetime, action])
-            print()
-            print('Acción agregada')
-            
+            os.system("cls||clear")
+            print("Acción agregada")
 
+        # show car log
         elif choice == 2:
-            print('*'*15, 'Bitácora del auto','*'*15)
+            os.system("cls||clear")
+            print(" Bitácora del auto ".center(60, "="))
             if len(bitacora) == 0:
-                print('No hay acciones registradas por el momento')
+                print("No hay acciones registradas por el momento")
             else:
-                for fecha,action in bitacora:
-                    print(fecha,action)
-            
+                for fecha, action in bitacora:
+                    print(fecha, action)
+
+        # save car log to csv file
         elif choice == 3:
-            file_name = input('Ingrese el nombre del archivo al cual desea guardar la bitácora\n> ')
-            file_name = file_name if len(file_name) > 0 else 'bitacora'
-            fieldnames = ['fecha', 'accion']
-            with open(f'{file_name}.csv', 'w+', newline='', encoding='utf-8') as csv_file:
-                writer = csv.DictWriter(csv_file,fieldnames=fieldnames)
+            os.system("cls||clear")
+            file_name = input(
+                "Ingrese el nombre del archivo al cual desea guardar la bitácora\n> "
+            )
+            file_name = file_name if len(file_name) > 0 else "bitacora"
+            fieldnames = ["fecha", "accion"]
+            with open(
+                f"{file_name}.csv", "w+", newline="", encoding="utf-8"
+            ) as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
                 writer.writeheader()
-                writer.writerows({'fecha': action[0], 'accion': action[1]} for action in bitacora)
-                print('Bitácora guardada con éxito')
+                writer.writerows(
+                    {"fecha": action[0], "accion": action[1]} for action in bitacora
+                )
+                os.system("cls||clear")
+                print("Bitácora guardada con éxito")
+
+        # exit program
         elif choice == 4:
-            print('Saliendo del menú')
+            print("Saliendo del menú")
             break
+
+        # handle invalid options
         else:
-            print('Favor ingresar una opción válida')
+            os.system("cls||clear")
+            print("\nFavor ingresar una opción válida\n")
         print()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()

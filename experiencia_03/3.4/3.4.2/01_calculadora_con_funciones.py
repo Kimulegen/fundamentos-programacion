540b86700e4c83376c24de474d3c82aa21de40a9
kimulegen <kimulegen@proton.me>
2024-06-07 15:11:32 -0400

    chore: add comments

--- experiencia_03/3.4/3.4.2/01_calculadora_con_funciones.py
+++ experiencia_03/3.4/3.4.2/01_calculadora_con_funciones.py
@@ -1,33 +1,44 @@
-def calculator(a,b):
-    def sum(a,b):
+#! Python3
+# A calculator function
+
+
+def calculator(a, b):
+    # define operation functions
+    def sum(a, b):
         return a + b
-    def rest(a,b):
+
+    def rest(a, b):
         return a - b
-    def mult(a,b):
+
+    def mult(a, b):
         return a * b
-    def divide(a,b):
+
+    def divide(a, b):
         try:
             return a / b
         except ZeroDivisionError:
-            print('No se puede dividir por cero!')
-            return 'invalid'
-            
+            print("No se puede dividir por cero!")
+            return "invalid"
 
-    operacion = input('Indique la operacion que desea hacer (sumar/restar/multiplicar/dividir)\n> ')
+    # ask for type of operation, show results
+    operacion = input(
+        "Indique la operacion que desea hacer (sumar/restar/multiplicar/dividir)\n> "
+    )
     match operacion:
         case "sumar":
-            print('La suma de los numeros es:', sum(a,b))
+            print("La suma de los numeros es:", sum(a, b))
         case "restar":
-            print('La resta de los numeros es:', rest(a,b))
+            print("La resta de los numeros es:", rest(a, b))
         case "multiplicar":
-            print('La multiplicacion de los numeros es:', mult(a,b))
+            print("La multiplicacion de los numeros es:", mult(a, b))
         case "dividir":
-            print('La division de los numeros es:', divide(a,b))
+            print("La division de los numeros es:", divide(a, b))
         case _:
-            print('operacion no valida')
+            print("operacion no valida")
             return
 
-a = int(input('Ingrese primer numero\n> '))
-b = int(input('Ingrese segundo numero\n> '))
 
-calculator(a,b)
+a = int(input("Ingrese primer numero\n> "))
+b = int(input("Ingrese segundo numero\n> "))
+
+calculator(a, b)
--- experiencia_03/3.4/3.4.2/02_calculadora_con_validacion.py
+++ experiencia_03/3.4/3.4.2/02_calculadora_con_validacion.py
@@ -1,42 +1,54 @@
-def calculator(a,b):
-    def sum(a,b):
+#! Python3
+# A calculator function and a validation function to sanitize user input
+
+
+# calculator function
+def calculator(a, b):
+    def sum(a, b):
         return a + b
-    def rest(a,b):
+
+    def rest(a, b):
         return a - b
-    def mult(a,b):
+
+    def mult(a, b):
         return a * b
-    def divide(a,b):
+
+    def divide(a, b):
         try:
             return a / b
         except ZeroDivisionError:
-            print('No se puede dividir por cero!')
-            return 'invalid'
-            
+            print("No se puede dividir por cero!")
+            return "invalid"
 
-    operacion = input('Indique la operacion que desea hacer (sumar/restar/multiplicar/dividir)\n> ')
+    operacion = input(
+        "Indique la operacion que desea hacer (sumar/restar/multiplicar/dividir)\n> "
+    )
     match operacion:
         case "sumar":
-            print('La suma de los numeros es:', sum(a,b))
+            print("La suma de los numeros es:", sum(a, b))
         case "restar":
-            print('La resta de los numeros es:', rest(a,b))
+            print("La resta de los numeros es:", rest(a, b))
         case "multiplicar":
-            print('La multiplicacion de los numeros es:', mult(a,b))
+            print("La multiplicacion de los numeros es:", mult(a, b))
         case "dividir":
-            print('La division de los numeros es:', divide(a,b))
+            print("La division de los numeros es:", divide(a, b))
         case _:
-            print('operacion no valida')
+            print("operacion no valida")
             return
 
+
+# validation function
 def validar(n):
     while True:
         try:
             fl = float(n)
             return fl
         except ValueError:
-            print('Error! ingrese un numero valido')
-            n = input('Reingrese el numero\n> ')
-            
-a = validar(input('Ingrese primer numero\n> '))
-b = validar(input('Ingrese segundo numero\n> '))
+            print("Error! ingrese un numero valido")
+            n = input("Reingrese el numero\n> ")
 
-calculator(a,b)
+
+a = validar(input("Ingrese primer numero\n> "))
+b = validar(input("Ingrese segundo numero\n> "))
+
+calculator(a, b)

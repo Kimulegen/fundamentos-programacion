e7c5055bd9bb3780b4127559846979fba319e065
kimulegen <kimulegen@proton.me>
2024-06-23 01:00:58 -0400

    feat:add rest of the program

--- experiencia_03/challenges/vip_tickets.py
+++ experiencia_03/challenges/vip_tickets.py
@@ -12,15 +12,7 @@ import os
 
 # initialize variables
 seats = list(range(1, 101))
-
-
-def sell_seat(seats, seat_number, rut):
-    if len(seats[seat_number - 1]) > 2:
-        print("No disponible")
-        return False
-    else:
-        seats[seat_number - 1] = rut
-    return True
+attendees = []
 
 
 def display_seats(seats):
@@ -40,6 +32,78 @@ def display_seats(seats):
             print(f"{i:02}", end=" ")
         counter += 1
     print("\n\n")
+
+
+def is_valid(rut_str):
+    if rut_str[0] == "0":
+        return False
+
+    if len(rut_str) < 7 or len(rut_str) > 8:
+        return False
+
+    try:
+        int(rut_str)
+    except ValueError:
+        return False
+
+    return True
+
+
+def calculate_total_profits(seats):
+    profits = 0
+    tickets_sold = {"platinum": 0, "gold": 0, "silver": 0}
+    for index, seat in enumerate(seats):
+        if isinstance(seat, str):
+            if index <= 19:
+                tickets_sold["platinum"] = tickets_sold["platinum"] + 1
+                profits = profits + 120000
+            elif index <= 49:
+                tickets_sold["gold"] = tickets_sold["gold"] + 1
+                profits = profits + 80000
+            else:
+                tickets_sold["silver"] = tickets_sold["silver"] + 1
+                profits = profits + 50000
+
+    print("-" * 54)
+    print(
+        "Tipo de Entrada".center(20, " "),
+        "|",
+        "Cantidad".center(10, " "),
+        "|",
+        "Total".center(20, " "),
+    ), print("-" * 54),
+    print(
+        "Platinum $120.000".ljust(20, " "),
+        "|",
+        f'{tickets_sold["platinum"]}'.center(10, " "),
+        "|",
+        f'${tickets_sold["platinum"]*120000}'.center(20, " "),
+    ), print("-" * 54),
+    print(
+        "Gold $80.000".ljust(20, " "),
+        "|",
+        f'{tickets_sold["gold"]}'.center(10, " "),
+        "|",
+        f'${tickets_sold["gold"]*8000}'.center(20, " "),
+    ), print("-" * 54),
+    print(
+        "Silver $50.000".ljust(20, " "),
+        "|",
+        f'{tickets_sold["silver"]}'.center(10, " "),
+        "|",
+        f'${tickets_sold["silver"]*50000}'.center(20, " "),
+    ), print("-" * 54),
+    print(
+        "Total.".ljust(20, " "),
+        "|",
+        f'{tickets_sold["platinum"]+tickets_sold["gold"]+tickets_sold["silver"]}'.center(
+            10, " "
+        ),
+        "|",
+        f'${tickets_sold["platinum"]*120000 + tickets_sold["gold"]*8000+tickets_sold["silver"]*50000} '.center(
+            20, " "
+        ),
+    ), print("-" * 54), print()
 
 
 def main():
@@ -95,9 +159,13 @@ def main():
                         print("No esta disponible")
                     else:
                         rut = input(
-                            "Ingresa el rut de la persona que va a utilizar ese asiento\n> "
+                            "Ingresa el rut de la persona que va a utilizar ese asiento\nSin digito verificador\n> "
                         )
+
+                        while not is_valid(rut):
+                            rut = input("ingresa un rut valido: ")
                         seats[seat_to_buy - 1] = rut
+                        attendees.append(int(rut))
                         purchase_summary.append({seat_to_buy: rut})
                         break
 
@@ -111,11 +179,15 @@ def main():
             print()
 
         elif choice == 2:
-            pass
+            display_seats(seats)
         elif choice == 3:
-            pass
+            print("Lista de asistentes")
+            print("-" * 20)
+            attendees.sort()
+            for attendee in attendees:
+                print(attendee)
         elif choice == 4:
-            pass
+            calculate_total_profits(seats)
         else:
             print("Saliendo del programa...")
             break
